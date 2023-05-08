c055b4778da13387c7d67601181cb0ae2403fd58
xixon0531 <xixon0531@gmail.com>
2023-05-08 10:16:26 +0900

    json Array

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <json-c/json.h>
 
-#define FILE_PATH "./student.json"
+#define FILE_PATH "student.json"
 
 typedef struct student{
 	char name[5];
@@ -12,25 +12,33 @@ typedef struct student{
 }student_t;
 
 json_object *add_student(char *name, unsigned int age, char *phone, char *parent_phone, char *class_time){
-	json_object *phone_num, *studentobj;
-	studentobj = json_object_new_object();
+	json_object *phone_num, *rootValue;
+
 	phone_num = json_object_new_object();
-	student_t student;
+    rootValue = json_object_from_file(FILE_PATH);
+    if (!rootValue) {
+        printf("Failed to parse JSON data.\n");
+        return 1;
+    }
 
-	json_object_object_add(studentobj, "name", json_object_new_string(name));
-	json_object_object_add(studentobj, "age", json_object_new_int(age));
+    json_object *student = json_object_array_get_idx(rootValue, 0);
+	json_object_object_add(student, "name", json_object_new_string(name));
+	json_object_object_add(student, "age", json_object_new_int(age));
 
-	//json_object_object_add(studentobj, "phone", json_object_new_string(phone));
 	json_object_object_add(phone_num, "own", json_object_new_string(phone));
 	json_object_object_add(phone_num, "p", json_object_new_string(parent_phone));
-	json_object_object_add(studentobj, "phone number", phone_num);
+	json_object_object_add(student, "phone number", phone_num);
 
-	json_object_object_add(studentobj, "class_time", json_object_new_string(class_time));
+	json_object_object_add(student, "class_time", json_object_new_string(class_time));
 
-	json_object_to_file(FILE_PATH, studentobj);
+    json_object_array_add(rootValue, student);
+    
+    json_object_to_file("student.json", rootValue);
 
-	return studentobj;
+	return rootValue;
 }
+
+
 
 int main(){
 	//사용할 기능의 번호 입력, 입력한 값에 따른 함수 사용
