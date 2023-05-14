0eaa8f4208602dabbccce4201423f74b3663ac86
seyeong <choe6562@gmail.com>
2023-05-15 06:12:09 +0900

    check

--- 1.c
+++ 1.c
@@ -4,91 +4,55 @@
 #include <time.h>
 #include <json-c/json.h>
 
-#define MAX_SUBJECTS 10
-
-typedef struct {
-    char start_time[10];
-    char end_time[10];
-} Subject;
-
-typedef struct {
-    char name[20];
-    char phone[20];
-    int age;
-    Subject subjects[MAX_SUBJECTS];
-    int attendance[MAX_SUBJECTS][31];
-} Student;
-
-int saved_attendance(Student* student, int subject_index, int day) {
-    if (subject_index < 0 || subject_index >= MAX_SUBJECTS) {
-        fprintf(stderr, "Invalid subject index.\n");
-        return -1;
-    }
-    if (day < 1 || day > 31) {
-        fprintf(stderr, "Invalid day.\n");
-        return -1;
-    }
-
-    time_t t = time(NULL);
-    struct tm* tm = localtime(&t);
-    char current_time[10];
-    strftime(current_time, sizeof(current_time), "%H:%M:%S", tm);
-
-    char* start_time = student->subjects[subject_index].start_time;
-    char* end_time = student->subjects[subject_index].end_time;
-
-    if (strcmp(current_time, start_time) < 0 || strcmp(current_time, end_time) > 0) {
-        // 현재 시간이 수업 시간이 아님
-        return 0;
-    } else {
-        // 출석 처리
-        student->attendance[subject_index][day - 1] = 1;
-        return 1;
-    }
-}
+#define STUDENT_FILE "json/student.json"
 
 int check_attendacne(const char* phone_number) {
-    FILE *fp = fopen("student.json", "r");
-    char buffer[1024];
-    fread(buffer, 1024, 1, fp);
-    fclose(fp);
+    json_object *rootValue;
 
-    // JSON 객체를 생성
-    json_object *json_obj = json_tokener_parse(buffer);
+    rootValue = json_object_from_file(STUDENT_FILE);
+	if (!rootValue){
+		printf("Failed to parse JSON data. \n");
+		return -1;
+	}
 
-    const char *search_value = &phone_number;
-    const char *search_key = "phone";
+    json_object *search_phone_number = NULL;
 
-    json_object *search_obj = json_object_object_get(json_obj, search_key);
+    json_object_object_foreach(rootValue, key, val){
+		if(json_object_equal(val, json_object_new_string(phone_number))){
+			search_phone_number = rootValue;
+			break;
+		}
+	}
+    
+    // JSON에서 attendacne 정보 가져오기
+    json_object *attendance_obj = json_object_object_get(search_phone_number, "attendance");
 
-    json_object *searched_objs[100];
-    int searched_objs_count = 0;
+    if (attendance_obj == NULL) {
+        printf("attendance information not found. \n");
+        return -1;
+    }
 
-    // JSON 객체 검색
-    int i;
-    for (i = 0; i < json_object_array_length(json_obj); i++) {
-        json_object *obj = json_object_array_get_idx(json_obj, i);
+    int attendance = json_object_get_int(attendance_obj);
 
-        // 검색된 값과 일치하는 경우, 해당 객체를 배열에 추가
-        if (json_object_get_string(search_obj) == search_value) {
-            searched_objs[searched_objs_count++] = obj;
+    if (attendance == -1) {
+        //출석 저장
+        json_object_object_add(search_phone_number, "attendance", json_object_new_int(1));
+
+        if (json_object_to_file(STUDENT_FILE, rootValue) != 0) {
+            printf("Failed to save JSON data. \n");
+            return -1;
         }
+
+        printf("attendance saved. \n");
     }
 
-    // 검색된 객체들 중에서 정보 추출
-    for (i = 0; i < searched_objs_count; i++) {
-        json_object *obj = searched_objs[i];
-
-        // 정보를 추출하는 코드
-        json_object *name_obj = json_object_object_get(obj, "name");
-        json_object *age_obj = json_object_object_get(obj, "age");
-        const char *name = json_object_get_string(name_obj);
-        int age = json_object_get_int(age_obj);
-        printf("Name: %s, Age: %d\n", name, age);
+    else {
+        printf("attendance: %d\n", attendance);
     }
+
 
     // JSON 객체 해제
-    json_object_put(json_obj);
+    json_object_put(rootValue);
 
     return 0;
 }
